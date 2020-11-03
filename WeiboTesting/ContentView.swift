77488558d299a3a994ea9885c24946f17daef52d
dizzyLee <dizzyLee@126.com>
2020-11-03 19:31:06 +0800

    Initial Commit

---
+++ WeiboTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeiboTesting
+//
+//  Created by 李祎帆 on 2020/11/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeiboTesting/WeiboTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeiboTestingApp.swift
+//  WeiboTesting
+//
+//  Created by 李祎帆 on 2020/11/3.
+//
+
+import SwiftUI
+
+@main
+struct WeiboTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
